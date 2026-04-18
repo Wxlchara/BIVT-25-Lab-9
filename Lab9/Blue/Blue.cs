8585ad462b8f765aaf77bc6609d35647366e19e0
Wxlchara <anton.iyj.anton@gmail.com>
2026-04-18 03:32:12 +0300

    Update Blue.cs

--- Lab9/Blue/Blue.cs
+++ Lab9/Blue/Blue.cs
@@ -1,3 +1,32 @@
-﻿namespace Lab9.Blue
+namespace Lab9.Blue
 {
+    public abstract class Blue
+    {
+        private string _input;
+
+        public string Input
+        {
+            get
+            {
+                return _input;
+            }
+            private set
+            {
+                _input = value;
+            }
+        }
+
+        protected Blue(string text)
+        {
+            Input = text;
+        }
+
+        public abstract void Review();
+
+        public virtual void ChangeText(string text)
+        {
+            Input = text;
+            Review();
+        }
+    }
 }
